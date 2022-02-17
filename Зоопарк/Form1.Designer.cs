13b84a6a1e4c6dade8c2ea4a937a0adde0ac893e
GolubtsovaSof <92886622+GolubtsovaSof@users.noreply.github.com>
2022-02-18 02:03:09 +0300

    Добавьте файлы проекта.

---
+++ Зоопарк/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace Зоопарк
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button10 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button3
+			// 
+			this.button3.BackgroundImage = global::Зоопарк.Properties.Resources.mouse_1974359_1920_1536x1074;
+			this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button3.Location = new System.Drawing.Point(24, 238);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(139, 116);
+			this.button3.TabIndex = 2;
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.BackgroundImage = global::Зоопарк.Properties.Resources.orel;
+			this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button4.Location = new System.Drawing.Point(24, 360);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(139, 105);
+			this.button4.TabIndex = 3;
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button5
+			// 
+			this.button5.BackgroundImage = global::Зоопарк.Properties.Resources.slon;
+			this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button5.Location = new System.Drawing.Point(24, 471);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(139, 101);
+			this.button5.TabIndex = 4;
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// button6
+			// 
+			this.button6.BackgroundImage = global::Зоопарк.Properties.Resources.utka;
+			this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button6.Location = new System.Drawing.Point(42, 578);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(108, 137);
+			this.button6.TabIndex = 5;
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
+			// 
+			// button2
+			// 
+			this.button2.BackgroundImage = global::Зоопарк.Properties.Resources.fish;
+			this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button2.Location = new System.Drawing.Point(24, 124);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(139, 108);
+			this.button2.TabIndex = 1;
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button1
+			// 
+			this.button1.BackgroundImage = global::Зоопарк.Properties.Resources.croc;
+			this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button1.Location = new System.Drawing.Point(24, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(139, 106);
+			this.button1.TabIndex = 0;
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox1.Location = new System.Drawing.Point(496, 108);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(716, 159);
+			this.textBox1.TabIndex = 6;
+			// 
+			// button7
+			// 
+			this.button7.Enabled = false;
+			this.button7.Location = new System.Drawing.Point(496, 401);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(716, 23);
+			this.button7.TabIndex = 7;
+			this.button7.Text = "Лети";
+			this.button7.UseVisualStyleBackColor = true;
+			this.button7.Click += new System.EventHandler(this.button7_Click);
+			// 
+			// button8
+			// 
+			this.button8.Enabled = false;
+			this.button8.Location = new System.Drawing.Point(496, 487);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(716, 23);
+			this.button8.TabIndex = 8;
+			this.button8.Text = "Плыви";
+			this.button8.UseVisualStyleBackColor = true;
+			this.button8.Click += new System.EventHandler(this.button8_Click);
+			// 
+			// button9
+			// 
+			this.button9.Enabled = false;
+			this.button9.Location = new System.Drawing.Point(496, 568);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(716, 23);
+			this.button9.TabIndex = 9;
+			this.button9.Text = "Беги";
+			this.button9.UseVisualStyleBackColor = true;
+			this.button9.Click += new System.EventHandler(this.button9_Click);
+			// 
+			// button10
+			// 
+			this.button10.Enabled = false;
+			this.button10.Location = new System.Drawing.Point(496, 644);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(716, 23);
+			this.button10.TabIndex = 10;
+			this.button10.Text = "Голос";
+			this.button10.UseVisualStyleBackColor = true;
+			this.button10.Click += new System.EventHandler(this.button10_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1253, 737);
+			this.Controls.Add(this.button10);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.MaximumSize = new System.Drawing.Size(1271, 784);
+			this.MinimumSize = new System.Drawing.Size(1271, 784);
+			this.Name = "Form1";
+			this.Text = "Зоопарк";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button10;
+	}
+}
+
---
+++ Зоопарк/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Зоопарк
+{
+	public partial class Form1 : Form
+	{
+		public string n;
+		List<Animal> Animals = new List<Animal>();
+		List<Button> buttons = new List<Button>();
+		List<Button> AbButtons = new List<Button>();
+
+		int IndChosen;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			AbButtons.Add(button7); AbButtons.Add(button8); AbButtons.Add(button9); AbButtons.Add(button10);
+		}
+		abstract class Animal
+		{
+			protected string Name;
+			public Animal (string n)
+			{
+				Name = n;
+			}
+			public abstract string Talk();
+		}
+		interface IFly
+		{
+			string Fly();
+		}
+		interface IRun
+		{
+			string Run();
+		}
+		interface ISwim
+		{
+			string Swim();
+		}
+		class Croc : Animal, ISwim, IRun
+		{
+			public Croc(string name) : base(name) { }
+			public string Swim()
+			{ return "Крокодил " + Name + " поплыл(а)!"; }
+			public string Run()
+			{ return "Крокодил " + Name + " побежал(а)!"; }
+			public override string Talk()
+			{ return "Привет, я крокодил " + Name + "!"; }
+		}
+		class Fish : Animal, ISwim
+		{
+			public Fish(string name) : base(name) { }
+			public string Swim()
+			{ return "Рыба " + Name + " поплыл(а)!"; }
+			public override string Talk()
+			{ return "Рыба " + Name + " говорит: ''bool bool bool. Знаете, почему рыба не клюет? Потому что у нее нет клюва...''"; }
+		}
+		class Mouse : Animal, IRun, ISwim
+		{
+			public Mouse(string name) : base(name) { }
+			public string Run()
+			{ return "Мышь " + Name + " побежал(а)!"; }
+			public string Swim()
+			{ return "Мышь " + Name + " поплыл(а)!"; }
+			public override string Talk()
+			{ return "Мышь "+Name+" говорит: ''3,14 3,14 3,14. Мужчины как мыши: отдельно смотришь — трогательный зверек, а как заведется дома — хочется отравить.''"; }
+		}
+		class Eagle : Animal, IFly
+		{
+			public Eagle(string name) : base(name) { }
+			public string Fly()
+			{ return "Орел " + Name + " полетел(а)!"; }
+			public override string Talk()
+			{ return "Орел " + Name + " говорит: ''Женщины, запомните: сколько времени на дятла не тратить - орла всё равно из него не сделаешь.''"; }
+		}
+		class Eleph : Animal, IRun, ISwim
+		{
+			public Eleph(string name) : base(name) { }
+			public string Run()
+			{ return "Слон " + Name + " побежал(а)!"; }
+			public string Swim()
+			{ return "Слон " + Name + " поплыл(а)!"; }
+			public override string Talk()
+			{ return "Слон " + Name + " говорит: ''Видел в цирке слона. Так вот, нихрена он не ходит по диагонали!''"; }
+		}
+		class Duck : Animal, IRun, ISwim, IFly
+		{
+			public Duck(string name) : base(name) { }
+			public string Run()
+			{ return "Утка " + Name + " побежала(а)!"; }
+			public string Swim()
+			{ return "Утка " + Name + " поплыл(а)!"; }
+			public string Fly()
+			{ return "Утка " + Name + " полетел(а)!"; }
+			public override string Talk()
+			{ return "Утка " + Name + " говорит: ''Возвращаются мужики с охоты. — Да—а, не пошла сегодня чего—то охота! — Да—а—а... То ли утки высоко летали, то ли мы собаку низко подбрасывали...''"; }
+		}
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Form2 f2 = new Form2();
+			f2.ShowDialog();
+			n = f2.n;
+			Croc croc = new Croc(n);
+			Animals.Add(croc);
+			Button b = new Button();
+			b.Text = "Крокодил "+n;
+			b.Width = 200;
+			buttons.Add(b);
+			AddButton(b);
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			Form2 f2 = new Form2();
+			f2.ShowDialog();
+			n = f2.n;
+			Fish fish = new Fish(n);
+			Animals.Add(fish);
+			Button b = new Button();
+			b.Text = "Рыба " + n;
+			b.Width = 200;
+			buttons.Add(b);
+			AddButton(b);
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			Form2 f2 = new Form2();
+			f2.ShowDialog();
+			n = f2.n;
+			Mouse mouse = new Mouse(n);
+			Animals.Add(mouse);
+			Button b = new Button();
+			b.Text = "Мышь " + n;
+			b.Width = 200;
+			buttons.Add(b);
+			AddButton(b);
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			Form2 f2 = new Form2();
+			f2.ShowDialog();
+			n = f2.n;
+			Eagle eagle = new Eagle(n);
+			Animals.Add(eagle);
+			Button b = new Button();
+			b.Text = "Орел " + n;
+			b.Width = 200;
+			buttons.Add(b);
+			AddButton(b);
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			Form2 f2 = new Form2();
+			f2.ShowDialog();
+			n = f2.n;
+			Eleph eleph = new Eleph(n);
+			Animals.Add(eleph);
+			Button b = new Button();
+			b.Text = "Слон " + n;
+			b.Width = 200;
+			buttons.Add(b);
+			AddButton(b);
+		}
+
+		private void button6_Click(object sender, EventArgs e)
+		{
+			Form2 f2 = new Form2();
+			f2.ShowDialog();
+			n = f2.n;
+			Duck duck = new Duck(n);
+			Animals.Add(duck);
+			Button b = new Button();
+			b.Text = "Утка " + n;
+			b.Width = 200;
+			buttons.Add(b);
+			AddButton(b);
+		}
+		public void AddButton(Button b)
+		{
+			for (int i = 0; i < buttons.Count; i++)
+			{
+				Controls.Add(b);
+				b.Location = new Point(150, 10 + i * 20);
+				b.Click += B_Click;
+			}
+		}
+
+		private void B_Click(object sender, EventArgs e)
+		{
+			foreach(Button b in AbButtons)
+			{
+				b.Enabled = false;
+			}
+			IndChosen = buttons.IndexOf(sender as Button);
+			textBox1.Text = Animals[IndChosen].Talk();
+			if (Animals[buttons.IndexOf(sender as Button)] is IFly)
+			{
+				AbButtons[0].Enabled = true;
+			}
+			if (Animals[IndChosen] is ISwim)
+			{
+				AbButtons[1].Enabled = true;
+			}
+			if (Animals[IndChosen] is IRun)
+			{
+				AbButtons[2].Enabled = true;
+			}
+			AbButtons[3].Enabled = true;
+		}
+
+		private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void button7_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = (Animals[IndChosen] as IFly).Fly();
+		}
+
+		private void button8_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = (Animals[IndChosen] as ISwim).Swim();
+		}
+
+		private void button9_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = (Animals[IndChosen] as IRun).Run();
+		}
+		private void button10_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = Animals[IndChosen].Talk();
+		}
+	}
+}
